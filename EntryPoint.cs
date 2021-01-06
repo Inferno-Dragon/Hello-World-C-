bb987e1fd75c99a3299a1c68e33ab4379a224411
Iain Jenkins <63315736+Inferno-Dragon@users.noreply.github.com>
2021-01-06 10:45:55 +0200

    First Commit

---
+++ EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello world");
+        Console.WriteLine();
+        Console.WriteLine("I am learning C#");
+    }
+}
+
